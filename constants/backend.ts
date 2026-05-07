8f3efb6b32fed1d835282b13f3cd901b634066b7
BobbyWoodard <bobbywoodard11@gmail.com>
2026-05-06 20:17:14 -0700

    Backend url options for dev and main

--- constants/backend.ts
+++ constants/backend.ts
@@ -1,3 +1,9 @@
+//Uncomment the backend you want to deploy to
+
+//Dev Backend
+//const backend = "https://kwestatest-d9a5ffgda2dtcxhb.westus3-01.azurewebsites.net/"
+
+//Main Backend
 const backend = "https://kwesta-fafrb3hxcrdhe2cu.westus3-01.azurewebsites.net/"
 
 export default backend;
