6ceaf9612620cbe0632b5d014d0a3c107f9e9abc
TyHagan808 <tyhagan@calpoly.edu>
2026-05-14 13:07:53 -0700

    Made user profile popup

--- styles/overlayStyle.ts
+++ styles/overlayStyle.ts
@@ -5,35 +5,49 @@ const CARD_WIDTH = screen_width * 0.8;
 const CARD_MARGIN = 16;
 
 const styles = StyleSheet.create({
-    container: {
-        ...StyleSheet.absoluteFillObject,
-        zIndex: 1000,
-    },
-    backdrop: {
-        ...StyleSheet.absoluteFillObject,
-        backgroundColor: "rgba(0,0,0,0.1)",
-        zIndex: 1,
-    },
-    overlay: {
-        position: "absolute",
-        marginTop: 85,
-        justifyContent: "flex-start",
-        zIndex: 2,
-    },
-    Card: {
-        width: CARD_WIDTH,
-        height: 200,
-        backgroundColor: "white",
-        borderRadius: 20,
-        padding: 20,
-        marginHorizontal: CARD_MARGIN / 2,
-        marginTop: 16,
-        overflow: "hidden",
-    },
-    Slider: {
-        flexDirection: "row",
-    },
-    author: { fontWeight: "600", fontSize: 18, marginBottom: 8 },
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 1000,
+  },
+  backdrop: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0,0,0,0.1)",
+    zIndex: 1,
+  },
+  overlay: {
+    position: "absolute",
+    marginTop: 85,
+    justifyContent: "flex-start",
+    zIndex: 2,
+  },
+  Card: {
+    width: CARD_WIDTH,
+    height: 200,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 20,
+    marginHorizontal: CARD_MARGIN / 2,
+    marginTop: 16,
+    overflow: "hidden",
+  },
+  Slider: {
+    flexDirection: "row",
+  },
+  author: { fontWeight: "600", fontSize: 18, marginBottom: 8 },
+  popupOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  popup: {
+    width: "85%",
+    maxHeight: "80%",
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 20,
+  },
 });
 
-export default { styles, CARD_WIDTH, CARD_MARGIN};
+export default { styles, CARD_WIDTH, CARD_MARGIN };
